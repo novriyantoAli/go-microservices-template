1de8a329519350208b6f496efbb36275a848da5f
rhein <novriyanto.ali@gmail.com>
2021-04-17 20:23:57 +0800

    add tool package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/novriyantoAli/go-microservices-template
+
+go 1.16
---
+++ tool/doc.go
@@ -0,0 +1,3 @@
+// Package tool mewakili pembungkus untuk pustaka pihak ketiga. Ini dapat meningkatkan atau memodifikasi beberapa fungsi
+// dari perpustakaan pihak ketiga atau buat antarmuka untuk itu.
+package tool
